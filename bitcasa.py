1e52f69056d99c0d0e67634887f27983f1ca32f6
Chris Elleman <chris@elleman.co.uk>
2014-08-18 23:00:40 +0000

    BROKEN, currently refactoring buffering, need to convert download to use streambuffer and put chunking logic into download_file_part

--- bitcasa.py
+++ bitcasa.py
@@ -53,27 +53,63 @@ class DownloadJob(workerpool.Job):
 		self.bcParent.aheadBuffer_mutex.release()
 
 
+class DownloadChunk(workerpool.Job):
+	def __init__(self, download_url, rangeHeaderKeys, bcParent, chunk_size, buffer_size, start_byte, end_byte):
+		# should execute within a mutex
+		self.download_url = download_url
+		self.rangeHeaderKeys = rangeHeaderKeys
+		self.bcParent = bcParent
+		self.chunk_size = chunk_size
+		self.buffer_size = buffer_size
+		self.start_byte = start_byte
+		self.end_byte = end_byte
+	def run(self):
+		#print "DownloadChunk Downloading file from URL: " + self.download_url
+		# ToDo - convert download to use io buffer
+		threadId = threading.current_thread().ident
+		print "Threads: "+str(threading.active_count())+" "+str(threadId)
+		downLoop = 1
+		while downLoop != 0:
+	                r3 = None
+       	        	try:
+                        	print "download_file_part create request"
+                        	r3 = self.bcParent.httpsPool.request("GET", self.download_url,headers=self.rangeHeader,retries=self.bcParent.retry)
+                        	print "download_file_part get data "+str(self.rangeHeader)+" connection used: "+str(self.bcParent.httpsPool.num_connections)
+				downLoop = 0
+                	except Exception as e:
+                        	print "Exception (DownloadJob): "+str(type(e))+" "+str(e)
+				downLoop += 1
+				if downLoop > 4:
+					print "Error: DownloadJob downLoop too high, can't download range:"+str(self.rangeHeader)
+                        		return
+		self.bcParent.aheadBuffer_mutex.acquire()
+                if self.bcParent.aheadBuffer[self.download_url+str(self.rangeHeader)].complete == 0:
+			print "download_file_part update buffer "+str(self.rangeHeader)+" size:"+str(self.size)
+			self.bcParent.aheadBuffer[self.download_url+str(self.rangeHeader)].data = r3.data
+			self.bcParent.aheadBuffer[self.download_url+str(self.rangeHeader)].complete = 1
+		self.bcParent.aheadBuffer_mutex.release()
+
+
 # Bitcasa Class
 class Bitcasa:
-	_ids = count(0)
 	httpsPool = None
 	aheadBuffer = dict()
 	aheadBuffer_mutex = threading.Lock()
 	bufferSizeCnt = dict()
 	bufferSizeCnt_mutex = threading.Lock()
-	NUM_SOCKETS = 30
-	NUM_WORKERS = 10
+	NUM_SOCKETS = 6
+	NUM_WORKERS = 5
 	retry = urllib3.util.Retry(read=3, backoff_factor=2)
+
 	# Start Client & Load Config
 	def __init__ (self, config_path):
-		self.id = self._ids.next()
 		# Config file
 		self.config_path = config_path
 		try:
 			with open(self.config_path, 'r') as config_file:
 				self.config = json.load(config_file)
 		except:
-			sys.exit("Could not find configuration file.")
+			sys.exit("Exception (Bitcasa:init): Could not find configuration file: "+self.config_path)
 		# Set configuation variables
 		self.api_url = self.config['api_url'].encode('utf-8')
 		self.client_id = self.config['client_id'].encode('utf-8')
@@ -81,39 +117,24 @@ class Bitcasa:
 		self.redirect_url = self.config['redirect_url'].encode('utf-8')
 		self.auth_token = self.config['auth_token'].encode('utf-8')
 		self.access_token = self.config['access_token'].encode('utf-8')
-		
-		# Adding support for File Cache
+		# Adding support for File Cache - ToDo remove requirement
 		self.cache_dir = self.config['cache_dir'].encode('utf-8')
-		# Check to see if cache_dir is valid & exists
 		if(self.cache_dir == None):
-			self.cache_dir = os.path.dirname(os.path.realpath(__file__)) + ".cache"
-			self.save_config()
+			sys.exit("Exception (Bitcasa:init): Could not find cache_dir config variable: ")
 		# Now Make sure it exists
 		if not os.path.exists(self.cache_dir):
-			os.makedirs(self.cache_dir)
-		
+			sys.exit("Exception (Bitcasa:init): cache_dir doesn't exist: "+self.cache_dir)
 		# See if we need our tokens
 		if(self.auth_token == "") or (self.access_token == ""):
 			return self.authenticate()
-
 		# Initiate Connection
 		self.api_host = urlparse(self.api_url).hostname
 		self.api_path = urlparse(self.api_url).path
+		#urllib3.connection.HTTPSConnection.default_socket_options + [(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1),]
+		#urllib3.connection.HTTPConnection.default_socket_options + [(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1),]
 		# Create Connection Pool
-		#urllib3.connection.HTTPSConnection.default_socket_options + [
-		#	(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1),
-		#]
-		#urllib3.connection.HTTPConnection.default_socket_options + [
-                #        (socket.IPPROTO_TCP, socket.TCP_NODELAY, 1),
-                #]
-
 		self.httpsPool = urllib3.HTTPSConnectionPool(self.api_host, maxsize=self.NUM_SOCKETS, timeout=urllib3.Timeout(connect=2.0, read=5.0))
-
 		return None
-
-	def save_config (self):
-		with open(self.config_path, 'w') as outfile:
-			json.dump(self.config, outfile, indent=4)
 
 	def authenticate (self):
 		print("### ENTER THE FOLLOWING URL IN A BROWSER AND AUTHORIZE THIS APPLICATION ###")
@@ -170,7 +191,7 @@ class Bitcasa:
 		return response
 
 	# File API Methods
-        def download_file_part (self, download_url, offset, size, total_size):
+	def download_file_part (self, download_url, offset, size, total_size):
 		rangeHeader = None
 		return_data = None
 		if (offset + size) > total_size:
@@ -190,26 +211,31 @@ class Bitcasa:
 		while endLoop != 0:
 			self.aheadBuffer_mutex.acquire()
 			if download_url+str(rangeHeader) in self.aheadBuffer:
+				# Wait to download the current requested block
 				self.aheadBuffer_mutex.release()
 				endLoop = 0
+				sleepCnt = 0
 				while self.aheadBuffer[download_url+str(rangeHeader)].complete != 1:
 					print "download_file_part RangeHeader sleep: "+str(rangeHeader)
-					time.sleep(1)
-				return_data = self.aheadBuffer[download_url+str(rangeHeader)].data
+					time.sleep(0.1)
+					sleepCnt += 1
 				self.aheadBuffer_mutex.acquire()
+				return_data = self.aheadBuffer[download_url+str(rangeHeader)].data
 				self.aheadBuffer.pop(download_url+str(rangeHeader), None)
 				self.aheadBuffer_mutex.release()
 			else:
 				self.aheadBuffer_mutex.release()
 				pool = workerpool.WorkerPool(size=self.NUM_WORKERS)
-				# ADD MULTIPLE GETS
 				if ((offset + size) > total_size) or (self.bufferSizeCnt[size] < 3):
+					# ADD SINGLE GET
 					self.aheadBuffer_mutex.acquire()
 					if (download_url+str(rangeHeader) not in self.aheadBuffer) and (rangeHeader != None):
 						print "download_file_part add range header: "+str(rangeHeader)+" Singleton size:"+str(size)
 						job = DownloadJob(download_url, rangeHeader, self, size)
 						pool.put(job)
 					self.aheadBuffer_mutex.release()
+					pool.shutdown()
+					pool.wait()
 				else:
 					# ADD MULTIPLE RANGES
 					print "Multiple add job: "+str(rangeHeader)+" size:"+str(size)+" count:"+str(self.bufferSizeCnt[size])
@@ -246,13 +272,25 @@ class Bitcasa:
 				if endLoop > 2:
 					print "Error: download_file_part endLoop too high"
 					endLoop = 0
-                return return_data
+		return return_data
 
 
-        def download_file_url (self, file_id, path, file_name, file_size):
+	def createBuffer (self, download_url, rangeHeader, chunk_size):
+		newBuf = namedtuple('newBuf', 'data, complete, chunk_size')
+		if str(rangeHeader) not in self.aheadBuffer:
+			print "Bitcasa:createBuffer create buffer:"+str(rangeHeader)+" size:"+str(chunk_size)
+			self.bcParent.aheadBuffer[self.download_url+str(rangeHeader)] = newBuf
+			self.bcParent.aheadBuffer[self.download_url+str(rangeHeader)].complete = 0
+			self.bcParent.aheadBuffer[self.download_url+str(rangeHeader)].chunk_size = chunk_size
+		else:
+			print "Error (Bitcasa:createBuffer) create buffer:"+str(rangeHeader)+" size:"+str(chunk_size)+" already exists"
+
+
+	def download_file_url (self, file_id, path, file_name, file_size):
 		file_url = self.api_path + "/files/"+file_id+"/"+ urllib.quote_plus(file_name) +"?access_token=" + self.access_token + "&path=/" + path
-                print "File URL: " + file_url
+		print "File URL: " + file_url
 		return file_url
+
 
 	def upload_file (self, path, file_name, file_size):
 		return
